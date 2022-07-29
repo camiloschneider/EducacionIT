7d6612ead508e934b95c4efd089c015df7e44daa
camiloschneider <camiloschneider1@yahoo.com.ar>
2022-07-29 15:54:38 -0300

    Mejora de ejercicio fibonacci

--- Modulo01/EjercicioFibonacci.py
+++ Modulo01/EjercicioFibonacci.py
@@ -1,12 +1,14 @@
 def fibo(num):
   list = []
-  for i in range(0, num):
-    if i == 0:
-      list.append(0)
-    elif i == 1:
-      list.append(1)
-    else:
-      list.append(list[-2]+list[-1])
-  return list
-
-print(fibo(15))
+  if num >=2:
+    for i in range(0, num):
+      if i == 0:
+        list.append(0)
+      elif i == 1:
+        list.append(1)
+      else:
+        list.append(list[-2]+list[-1])
+    return list
+  return print('Debe ingresar un argumento mayor a 2')
+ 
+print(fibo(2))
